4e84ac2018945309356b361004cb888ab383a8f1
SomnolentLilac <addison.rogg@gmail.com>
2024-04-19 22:26:18 -0600

    Actual Final commit

---
+++ Coding Final/Assets/Scripts/List of Scripts PLEASE READ.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListofScripts : MonoBehaviour
+{
+    //Player Health: Health.cs
+    //Enemy and Projectiles: EnemyShoot.cs, Projectile.cs
+
+}
